a20e720f7b49bd8209d4b59fea238f1456b80fda
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-04-09 10:58:07 -0400

    14th lesson in regex-adventure

--- 14-blink.js
+++ 14-blink.js
@@ -18,3 +18,53 @@ For this task, use the `marked` module: `npm install marked`
 To verify your solution, run:
 
   regex-adventure verify solution.js
+
+
+
+incorrect solution:
+
+module.exports = function (str) {
+  /* return html string */
+  return str.match(/<blink>[^@@]*</blink>/g)
+}
+
+
+  TAP version 13
+# (anonymous)
+not ok 1 SyntaxError: Invalid regular expression flags
+  ---
+    operator: error
+    expected: undefined
+    actual:   [SyntaxError: Invalid regular expression flags]
+    stack:
+      /Users/adrianshepelavey/nodeschool/regex-adventure/blink.js:3
+      return str.match(/<blink>[^@@]*</blink>/g)
+                       ^
+
+    SyntaxError: Invalid regular expression flags
+        at createScript (vm.js:56:10)
+        at Object.runInThisContext (vm.js:97:10)
+        at Module._compile (module.js:542:28)
+        at Object.Module._extensions..js (module.js:579:10)
+        at Module.load (module.js:487:32)
+        at tryModuleLoad (module.js:446:12)
+        at Function.Module._load (module.js:438:3)
+        at Module.require (module.js:497:17)
+        at require (internal/module.js:20:19)
+        at /usr/local/lib/node_modules/regex-adventure/problems/blink/index.js:10:11
+  ...
+not ok 2 plan != count
+  ---
+    operator: fail
+    expected: 4
+    actual:   1
+  ...
+
+1..2
+# tests 2
+# pass  0
+# fail  2
+
+#########################################
+###   YOUR SOLUTION IS NOT CORRECT!   ###
+#########################################
